5c72a9e0f77f78d01f4e6f261e82f1bf6ff682d6
Tristan Kundrat <tristan.kundrat@gmail.com>
2020-09-30 14:59:42 +0200

    added files

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>unter konstruktion bois</title>
+		<style>
+			@import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
+			div a img{ visibility:hidden; }
+		</style>
+	</head>
+	<body style="background-color:#212121; color:#ffffff;">
+		<h3 style="text-align:center;">tristan420.de</h3>
+		<h1 style="text-align:center;">Diese Seite ist unter Konstruktion!</h1><br>
+		<br><br>
+		schaut euch doch das mal an lul:
+		<br><br>
+		<p style="font-family:'VT323'; font-size:18px;">C:\Users\Guest> <a href="http://twitter.tristan420.de/" style="color:#2eddff;">start http://twitter.tristan420.de/</a></p>
+
+		<p style="font-family:'VT323'; font-size:18px;">C:\Users\Guest> <a href="http://reddit.tristan420.de/" style="color:#2eddff;">start http://reddit.tristan420.de/</a></p>
+
+		<p style="font-family:'VT323'; font-size:18px;">C:\Users\Guest> <a href="/wolf.php" style="color:#2eddff;">start http://wolf.tristan420.de/</a></p>
+	</body>
+</html>
---
+++ wolf.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>haha du opfer hast kein blacc aies</title>
+		<style>
+			div a img { visibility:hidden; }
+		</style>
+	</head>
+	<body style="background-color:#212121; color:#ffffff;">
+		<h1>wolf hat kein blek eis lull</h1>
+		<img src="/wolf.png"></img>
+	</body>
+</html>
